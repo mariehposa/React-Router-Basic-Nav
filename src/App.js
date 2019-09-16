5d6457360946ae87b7ceeb9941b5cd220edce10b
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-16 12:22:22 +0100

    create Route path for home, contact and about

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ const App = () => (
   <div>
     <Navigation />
 
-    <Route path="" component=""/>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About}/>
+    <Route path="/contact" component={Contact}/>
   </div>
 );
 
